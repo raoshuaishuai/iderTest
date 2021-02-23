e83c50f8c7a7ccdffeda369104b662f1f646cfc2
zs <admin@qq.com>
2021-02-23 13:47:22 +0800

    增加了一个test2类

---
+++ src/test/Test2.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * 测试
+ *
+ * @author 饶庭祥 on 2021/2/23
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("这是"+i);
+        }
+    }
+}
